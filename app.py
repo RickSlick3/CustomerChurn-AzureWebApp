1e0efaac084d441aa3a38e0b63ef2e6a04a1ba0f
rickslick3 <richard.robertsf@gmail.com>
2024-12-03 18:08:11 -0500

    flask app to show data in a table

---
+++ app.py
@@ -0,0 +1,35 @@
+import pyodbc as odbc
+import pandas as pd
+from flask import Flask, render_template
+
+# server = 'cloudfinaldb.database.windows.net'
+# database = 'customerData'
+connection_string = 'Driver={ODBC Driver 18 for SQL Server};Server=tcp:cloudfinaldb.database.windows.net,1433;Database=customerData;Uid=CloudSA7fa8b584;Pwd=Pa$$w0rd!;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+
+conn = odbc.connect(connection_string)
+
+sql = '''SELECT * FROM BankChurners'''
+cursor = conn.cursor()
+cursor.execute(sql)
+dataset = cursor.fetchall()
+
+df = pd.read_sql("SELECT * FROM BankChurners", conn)
+
+# columns = [col[0] for col in cursor.description]
+# df = pd.DataFrame(dataset, columns=columns)
+# print(df)
+
+csv_file_path = 'bank_churners_data.csv'  # Specify your file path here
+df.to_csv(csv_file_path, index=False)
+
+app = Flask(__name__)
+
+@app.route('/')
+def display_csv():
+    table_html = df.to_html(classes="table table-bordered", index=False)  # 'table table-bordered' adds Bootstrap styling
+    
+    # Render the HTML template and pass the table_html
+    return render_template('index.html', table_html=table_html)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8000, debug=True)

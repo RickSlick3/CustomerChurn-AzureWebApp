ef56d42c3aa27efe9d6f0ba44fa5d1030f653be9
rickslick3 <richard.robertsf@gmail.com>
2024-12-05 20:36:26 -0500

    start of dashboard

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from dotenv import load_dotenv
 import os
 from utils.df_utils import *
 
-# import mpld3
-# import matplotlib.pyplot as plt
-# import numpy as np
-
 app = Flask(__name__)
 
 DEFAULT_ROWS = 400
@@ -20,6 +16,8 @@ if not app.config['CONNECTION_STRING']:
     raise ValueError("CONNECTION_STRING is not set in the .env file.")
 
 df = create_df_from_db(app.config['CONNECTION_STRING'])
+
+mapped_df = set_up(df)
 
 csv_file_path = 'utils/bank_churners_data.csv'
 create_csv(df, csv_file_path)
@@ -35,12 +33,10 @@ def form():
         return redirect(url_for("welcome", username=username))
     return render_template("form.html")
 
-
 @app.route("/welcome")
 def welcome():
     username = request.args.get("username", "Guest")
     return render_template("welcome.html", username=username)
-
 
 @app.route("/data", methods=["GET", "POST"])
 def display_data():
@@ -59,23 +55,43 @@ def display_data():
     df_html = filtered_df.to_html(index=True)
     return render_template("data.html", table=df_html)
 
-
 @app.route("/alldata")
 def display_all_data():
     df_html = df.to_html(index=True)
     return render_template("alldata.html", table=df_html)
 
-
-@app.route("/plot")
-def plot():
-    img = corr_heatmap(df)
-    return Response(img, mimetype='image/png')
-
-
 @app.route("/dashboard")
 def dashboard():
     return render_template('dashboard.html')
 
+@app.route("/corrheatmap")
+def corrheatmap():
+    img = corr_heatmap(mapped_df)
+    return Response(img, mimetype='image/png')
+
+@app.route("/income")
+def income():
+    img = churn_by_income(mapped_df)
+    return Response(img, mimetype='image/png')
+
+@app.route("/corrdict")
+def corrdict():
+    img = corr_dict(mapped_df)
+    return Response(img, mimetype='image/png')
+
+@app.route("/genderattrition")
+def genderattrition():
+    img = gender_attrition(mapped_df)
+    return Response(img, mimetype='image/png')
+
+@app.route("/relstatusattrition")
+def relstatusattrition():
+    img = rel_status_attrition(mapped_df)
+    return Response(img, mimetype='image/png')
+
+@app.route("/churn")
+def churn():
+    return
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=os.getenv('FLASK_ENV') == 'development')
--- utils/df_utils.py
+++ utils/df_utils.py
@@ -41,16 +41,134 @@ def sort_by_column(df, column_name):
         return df.sort_values(by=column_name)
     return df
 
-# example heatmap for dashboard
+def set_up(df):
+    # encode labels
+    mappings = {'Attrition_Flag' : {'Existing Customer': 0, 'Attrited Customer': 1},
+                'Gender' : {'M': 0, 'F': 1},
+                'Education_Level': {'Uneducated': 0, 'High School': 1, 'College': 2, 'Graduate': 3, 'Post-Graduate': 4, 'Doctorate': 5, 'Unknown': 6},
+                'Marital_Status': {'Single': 0, 'Married': 1, 'Divorced': 2, 'Unknown': 3},
+                'Income_Category' : {'Less than $40K': 0, '$40K - $60K': 1, '$60K - $80K': 2, '$80K - $120K': 3, '$120K +': 4, 'Unknown': 5},
+                'Card_Category' : {'Blue': 0, 'Silver': 1,'Gold': 2, 'Platinum': 3}}
+    
+    for column, mapping in mappings.items():
+        df[column] = df[column].map(mapping)
+
+    return df
+
+# correltion heatmap
 def corr_heatmap(df):
-    numerical_columns = df.select_dtypes(include=['float64', 'int64']).columns
-    df_numeric = df[numerical_columns]
-    correlation_matrix = df_numeric.corr()
+    correlation_matrix = df.corr()
     mask = np.triu(np.ones_like(correlation_matrix, dtype=bool))
     plt.figure(figsize=(12, 10))
-    sns.heatmap(correlation_matrix, mask=mask, annot=True, fmt='.2f', cmap='coolwarm', 
-                cbar_kws={"shrink": 0.8}, linewidths=0.5)
+    sns.heatmap(correlation_matrix, mask=mask, annot=True, fmt='.2f', cmap='coolwarm', cbar_kws={"shrink": 0.8}, linewidths=0.5)
     plt.title('Correlation Matrix')
+
+    img = io.BytesIO()
+    plt.savefig(img, format='png', bbox_inches='tight')
+    img.seek(0)
+    plt.close()
+    return img
+
+# Churn by income
+def churn_by_income(df):
+    income_attrition = df.groupby('Income_Category')['Attrition_Flag'].mean()
+    plt.figure(figsize=(7, 3))
+    plt.bar(['<40K', '40K-60K', '60K-80K', '80K-120K', '>120K', 'Unknown'], income_attrition, color='green')
+    plt.title('Churn by Income Category')
+    plt.xlabel('Income Category')
+    plt.ylabel('Attrition Rate (Proportion)')
+    plt.xticks(rotation=45)
+
+    img = io.BytesIO()
+    plt.savefig(img, format='png', bbox_inches='tight')
+    img.seek(0)
+    plt.close()
+    return img
+
+# dictionary for attrition correlation
+def corr_dict(df):
+    target_variable = 'Attrition_Flag'
+    correlation_dict = {}
+
+    #Fill the correlation dict
+    for column in df.columns:
+        if column != target_variable:  # Exclude the target variable itself
+            correlation = df[column].corr(df[target_variable])
+            correlation_dict[column] = correlation
+
+    keys = list(correlation_dict.keys())
+    values = list(correlation_dict.values())
+
+    plt.figure(figsize=(15, 4))
+    plt.bar(keys, values, color='skyblue')
+    plt.title('Attrition Correlation')
+    plt.xlabel('Columns')
+    plt.ylabel('Correlation')
+    plt.xticks(rotation=90)
+    plt.axhline(0, color='black', linewidth=1, linestyle='-')
+
+    img = io.BytesIO()
+    plt.savefig(img, format='png', bbox_inches='tight')
+    img.seek(0)
+    plt.close()
+    return img
+
+# Attrition and gender
+def gender_attrition(df):
+    gender_attrition = df.groupby(['Gender', 'Attrition_Flag']).size().unstack()
+    labels = ['Stayed', 'Left']
+
+    # Plot 1: Proportion of Male customers who stayed vs. left
+    plt.figure(figsize=(11, 3))
+
+    plt.subplot(1, 3, 1)
+    male_proportions = gender_attrition.loc[0] / gender_attrition.loc[0].sum()
+    plt.pie(male_proportions, labels=labels, autopct='%1.1f%%', startangle=90, colors=['lightblue', 'orange'])
+    plt.title('Attrition of male customers')
+
+    # Plot 2: Proportion of Female customers who stayed vs. left
+    plt.subplot(1, 3, 2)
+    female_proportions = gender_attrition.loc[1] / gender_attrition.loc[1].sum()
+    plt.pie(female_proportions, labels=labels, autopct='%1.1f%%', startangle=90, colors=['lightpink', 'teal'])
+    plt.title('Attrition of female customers')
+
+    img = io.BytesIO()
+    plt.savefig(img, format='png', bbox_inches='tight')
+    img.seek(0)
+    plt.close()
+    return img
+
+# marital status and attrition
+def rel_status_attrition(df):
+    # Group by Marital_Status and Attrition_Flag and count occurrences
+    marital_attrition = df.groupby(['Marital_Status', 'Attrition_Flag']).size().unstack()
+
+    labels = ['Stayed', 'Left']
+
+    # Plot setup
+    plt.figure(figsize=(11, 3))
+
+    # Plot 1: Proportion of Single customers who stayed vs. left
+    plt.subplot(1, 3, 1)
+    single_proportions = marital_attrition.loc[0] / marital_attrition.loc[0].sum()  # Single status: 0
+    plt.pie(single_proportions, labels=labels, autopct='%1.1f%%', startangle=90, colors=['lightblue', 'orange'])
+    plt.title('Attrition of Single customers')
+
+    # Plot 2: Proportion of Married customers who stayed vs. left
+    plt.subplot(1, 3, 2)
+    married_proportions = marital_attrition.loc[1] / marital_attrition.loc[1].sum()  # Married status: 1
+    plt.pie(married_proportions, labels=labels, autopct='%1.1f%%', startangle=90, colors=['lightgreen', 'red'])
+    plt.title('Attrition of Married customers')
+
+    # Plot 3: Proportion of Divorced customers who stayed vs. left
+    plt.subplot(1, 3, 3)
+    divorced_proportions = marital_attrition.loc[2] / marital_attrition.loc[2].sum()  # Divorced status: 2
+    plt.pie(divorced_proportions, labels=labels, autopct='%1.1f%%', startangle=90, colors=['lightcoral', 'purple'])
+    plt.title('Attrition of Divorced customers')
+
+    # Adjust layout for better display
+    plt.tight_layout()
+
     img = io.BytesIO()
     plt.savefig(img, format='png', bbox_inches='tight')
     img.seek(0)
